07965fb5e9f49e0334095e6e016d96b32b0be636
Kidus Tekeste <kidusmamuye@gmail.com>
2021-04-28 08:50:25 -0500

    Lab7:Q1 done with reason why

--- Lab7/Question1.js
+++ Lab7/Question1.js
@@ -23,3 +23,13 @@ let user = {
 }
 //Passed the context user inside "bind" for passed method references
 askPassword(user.loginOk.bind(user), user.loginFail.bind(user));
+askPassword(()=>user.loginOk.call(user), ()=>user.loginFail.call(user));
+askPassword(()=>user.loginOk.apply(user), ()=>user.loginFail.apply(user));
+
+// Reason why it didn't work? with the below code is:
+// (old)    askPassword(user.loginOk, user.loginFail);
+// is because the "this" statement called both in "loginOk" and "loginFail"
+// uses this reference which will return error when being passed as a reference
+// and called in a different execution environment. Therefore, we need to 
+// pass in the "user" object so the methods will this statement will be 
+// found in the new environment as well.
