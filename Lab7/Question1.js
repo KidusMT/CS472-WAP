416c93ba6e402eb7a958b67fd078885fcc6a1a35
Kidus Tekeste <kidusmamuye@gmail.com>
2021-04-28 08:44:48 -0500

    Lab7: Q1 done

--- Lab7/Question1.js
+++ Lab7/Question1.js
@@ -1,20 +1,25 @@
-function askPassword(ok, fail){
+/**
+ * Passed the context user inside "bind" for passed method references
+ * @param {*} ok -- function reference passed 
+ * @param {*} fail -- function reference passed 
+ */
+function askPassword(ok, fail) {
     let password = prompt("Password?", '');
-    if(password == "rockstar") ok();
+    if (password == "rockstar") ok();
     else fail();
 }
 
 let user = {
     name: 'John',
 
-    loginOk(){
-        alert('${this.name} logged in');
+    loginOk() {
+        alert(`${this.name} logged in`);
     },
 
-    loginFail(){
-        alert('${this.name} failed to log in');
+    loginFail() {
+        alert(`${this.name} failed to log in`);
     }
 
 }
-
-askPassword(user.loginOk, user.loginFail);
+//Passed the context user inside "bind" for passed method references
+askPassword(user.loginOk.bind(user), user.loginFail.bind(user));
