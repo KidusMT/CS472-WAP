499ec2b7f83722f49b8889ed4409b4930ff0ac2f
Kidus Tekeste <kidusmamuye@gmail.com>
2021-04-29 23:24:32 -0500

    Lab8: updates on Q2 and Q1

--- Lab8/Question1.js
+++ Lab8/Question1.js
@@ -5,8 +5,8 @@ let student = {
     inputNewGrade: function (newGrade) {
         this.grades.push(newGrade);
     },
-    computeAverageGrade : function () {
-        return this.grades.reduce((avg, value, _, { length }) => avg + value / length , 0);
+    computeAverageGrade: function () {
+        return this.grades.reduce((avg, value, _, { length }) => avg + value / length, 0);
     }
 }
 /**
@@ -17,23 +17,23 @@ let student = {
 let std1 = Object.create(student);
 std1.firstName = 'Kidus';
 std1.lastName = 'Tekeste';
-std1.grades = [3.7,4,3.8,4];
+std1.grades = [3.7, 4, 3.8, 4];
+std1.inputNewGrade(3.9);
 
 let std2 = Object.create(student);
 std2.firstName = 'Daniel';
 std2.lastName = 'Mathew';
-std2.grades = [2,3,4,1];
+std2.grades = [2, 3, 4, 1];
+std2.inputNewGrade(2.9);
 
 let std3 = Object.create(student);
 std3.firstName = 'Andrew';
 std3.lastName = 'Thomson';
-std3.grades = [3.3,4,3.1,4];
+std3.grades = [3.3, 4, 3.1, 4];
+std3.inputNewGrade(1.9);
 
 let students = [std1, std2, std3];
 
-let avgGrades = 0;
-for(let i = 0; i< students.length; i++){
-    avgGrades+=students[i].computeAverageGrade();
+for (let i = 0; i < students.length; i++) {
+    console.log(`${students[i].firstName + " " + students[i].lastName}: has average grade of ${students[i].computeAverageGrade()}`);
 }
-
-console.log(avgGrades/students.length);
--- Lab8/Question2.js
+++ Lab8/Question2.js
@@ -4,10 +4,10 @@
  */
 
 function Student(firstName, lastName, grades){
-    this.firstName= firstName,
-    this.lastName= lastName,
-    this.grades= grades,
-    this.inputNewGrade= function (newGrade) {
+    this.firstName = firstName,
+    this.lastName = lastName,
+    this.grades = grades,
+    this.inputNewGrade = function (newGrade) {
         this.grades.push(newGrade);
     },
     this.computeAverageGrade = function () {
@@ -16,14 +16,16 @@ function Student(firstName, lastName, grades){
 }
 
 let std1 = new Student('Kidus', 'Tekeste', [3.7,4,3.8,4]);
+std1.inputNewGrade(3.9);
+
 let std2 = new Student('Daniel', 'Mathew', [2,3,4,1]);
+std2.inputNewGrade(2.9);
+
 let std3 = new Student('Andrew', 'Thomson', [3.3,4,3.1,4]);
+std3.inputNewGrade(1.9);
 
 let students = [std1, std2, std3];
 
-let avgGrades = 0;
-for(let i = 0; i< students.length; i++){
-    avgGrades+=students[i].computeAverageGrade();
+for (let i = 0; i < students.length; i++) {
+    console.log(`${students[i].firstName + " " + students[i].lastName}: has average grade of ${students[i].computeAverageGrade()}`);
 }
-
-console.log(avgGrades/students.length);
